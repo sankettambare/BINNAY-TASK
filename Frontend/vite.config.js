b2ccc23f038bee5ab87d5c78156097cbdb8c56ad
Sanket Tambare <sankettambare24@gmail.com>
2026-01-16 20:15:18 +0530

    Fix Netlify build

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/BINNAY-TASK/',
+  base: '/',          // ✅ MUST be '/'
   build: {
     outDir: 'docs'
   }
