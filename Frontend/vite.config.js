0e933de32fede3bb591e7cb971b94040b8e925fa
Sanket Tambare <sankettambare24@gmail.com>
2026-01-16 11:47:25 +0530

    Add docs build for GitHub Pages

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/BINNAY-TASK/',   // 👈 repo name (CASE-SENSITIVE)
+  build: {
+    outDir: 'docs'        // 👈 VERY IMPORTANT
+  }
 })
